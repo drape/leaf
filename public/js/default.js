83d12854e58297104fa2d289e0f49a3be623fc1d
Dennis Malmin <denmalmin@gmail.com>
2015-10-13 17:08:33 +0200

    Add socket.io also setting up a very basic chat room.

--- public/js/default.js
+++ public/js/default.js
@@ -2,10 +2,14 @@ $(document).ready( function() {
   console.log('Document is ready.');
 
   $(window).scroll( function() {
-    if ($(this).scrollTop() >= 300) {
+    if ($(this).scrollTop() >= 100) {
       $('header').addClass('background-color');
     } else {
       $('header').removeClass('background-color');
     }
+  });
+
+  $('.nav ul li a').hover( function() {
+    $('.slider').animate({'width': 'slow'});
   });
 });
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var path = require('path');
 var routes = require('./routes');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,6 +18,25 @@ app.get('*', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-    console.log('Now listening on port: 3000')
+io.on('connection', function(socket) {
+  console.log('User connected');
+  socket.on('disconnect', function(){
+    console.log('User disconnected')
+  });
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    console.log('message ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('Listening on *:3000');
 });
